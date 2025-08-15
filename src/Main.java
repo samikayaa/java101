47c666bcd16011020b162186830a776321853946
samikaya <samikaya89@gmail.com>
2025-08-15 14:33:58 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//Aşağıdaki yönergeleri gerçekleştiriniz.
+// 1-)number isminde int tipinde bir değişken tanımla ve değerini 10 olarak ata.
+// 2-)decimalNumber isminde double tipinde bir değişken tanımla ve değerini 20.5 olarak ata.
+// 3-)isJavaFun isminde boolean tipinde bir değişken tanımla ve değerini true olarak ata.
+
+
+public class Main {
+    public static void main(String[] args) {
+        int number = 10;
+        double decimalNumber = 20.5;
+        boolean isJavaFun = true;
+    }
+}
+
+
---
+++ src/Main02.java
@@ -0,0 +1,9 @@
+//strNumber değişkenini integer bir değere cevirip number isminde bir değişkene atayınız.
+
+public class Main02 {
+    public static void main(String[] args) {
+        String strNumber = "5100";
+        int number = Integer.parseInt(strNumber);
+        System.out.println(number);
+    }
+}
