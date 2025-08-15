e56a55ff23a96d82cde902b933463abe5ec75a1b
samikaya <samikaya89@gmail.com>
2025-08-15 14:41:08 +0300

    First commit

---
+++ src/Main03.java
@@ -0,0 +1,17 @@
+/* Aşağıdaki kod çalıştırıldığında
+StringConcat fonksiyonu 2 değişkeni de string beklediğinden dolayı hata almaktadır.
+firstNum değişkenini parametre olarak String'e cevirerek yollayınız.
+ */
+
+public class Main03 {
+    public static void main(String[] args) {
+        String message = "Öğrencinin sınav notu: ";
+        int firstNum = 100;
+        String firstNumStr = Integer.toString(firstNum);
+        stringConcat(message, firstNumStr);
+    }
+
+    public static void stringConcat(String firstWord, String secondWord){
+        System.out.println(firstWord + " " + secondWord);
+    }
+}
